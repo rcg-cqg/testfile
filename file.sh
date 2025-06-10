e2f50aa0ecb2bfc46ea54bab62aee7161551e065
rcg-cqg <r.guiao@cyberqgroup.com>
2025-06-10 10:15:47 +0800

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,1 +1,16 @@
-#testfile
+#!/bin/bash
+
+# Simulated attacker command
+ping -c 1 127.0.0.1 > /dev/null && clear
+
+# Visual indicator of "success"
+echo -e "\e[31m"
+echo "######################################"
+echo "#                                    #"
+echo "#         You've been hacked!        #"
+echo "#                                    #"
+echo "######################################"
+echo -e "\e[0m"
+
+# Optional: Drop a marker file
+touch /tmp/pwned.txt
